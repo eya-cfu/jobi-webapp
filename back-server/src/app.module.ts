a7d0571c85701f9717f5878b2d54598b7b66b13a
eya-cfu <zaouieya@gmail.com>
2021-05-11 16:36:22 +0100

    Revert "entities added"
    This reverts commit da45e569433f9ea49b61482daf92059b82e59791.
    
    undoing entities to redo them with database type orm

--- back-server/src/app.module.ts
+++ back-server/src/app.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'test',
-      entities: [[__dirname + '/**/*.entity{.ts,.js}']],
-      synchronize: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
