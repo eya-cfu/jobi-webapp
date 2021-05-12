e3f54d878ee313082a6259a3e022394404ea586d
unknown <ayadi.yassine15@gmail.com>
2021-05-12 22:41:20 +0100

    JobOffer  Model added

---
+++ webProject/src/app/Models/job-offer.ts
@@ -0,0 +1,4 @@
+export class JobOffer {
+    constructor(public OfferId =0, public title='', public datePosted='', public status='', public employmentType='', public category='' , public location='', public salary='', public requirements='', public responsabilities='', public about='', public hoursPerWeek=0, public languages='',companyId=0,employerId=0){}
+
+}
