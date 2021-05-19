85286e2c807ae85cc00238b52fa0aa99d6cd4c9b
eya-cfu <zaouieya@gmail.com>
2021-05-19 20:38:15 +0100

    users endpoints, login missing

--- back-server/src/users/users.controller.ts
+++ back-server/src/users/users.controller.ts
@@ -8,35 +8,34 @@ import {
   Delete,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UserDto } from './user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  create(@Body() data: UserDto) {
+    return this.usersService.createUser(data);
   }
 
   @Get()
   findAll() {
-    return this.usersService.findAll();
+    return this.usersService.getAllUsers();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    return this.usersService.getUserById(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  update(@Param('id') id: number, @Body() data: UserDto) {
+    return this.usersService.updateUser(id, data);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  remove(@Param('id') id: number) {
+    return this.usersService.deleteUser(id);
   }
 }
--- back-server/src/users/users.service.ts
+++ back-server/src/users/users.service.ts
@@ -1,26 +1,48 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { UserDto } from './user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+  ) {}
+
+  async createUser(data: UserDto) {
+    const { email } = data;
+    let user = await this.userRepo.findOne({ where: { email } });
+    if (user) {
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+    }
+    user = this.userRepo.create(data);
+    await this.userRepo.save(user);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async getAllUsers(): Promise<User[]> {
+    return await this.userRepo.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async getUserById(id: number): Promise<User> {
+    const user = await this.userRepo.findOne(id);
+    if (!user) {
+      throw new HttpException('User does not exist', HttpStatus.NOT_FOUND);
+    }
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async updateUser(id: number, data: UserDto): Promise<UpdateResult> {
+    let user = await this.userRepo.findOne(id);
+    if (!user) {
+      throw new HttpException('User does not exist', HttpStatus.NOT_FOUND);
+    }
+    user = this.userRepo.create(data);
+    return await this.userRepo.update(id, user);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async deleteUser(id: number): Promise<DeleteResult> {
+    return await this.userRepo.delete(id);
   }
 }
