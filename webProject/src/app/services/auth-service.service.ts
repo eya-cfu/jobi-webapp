ec8ba99c20dbb169adf35facf893f6eaf0fc26bd
unknown <ayadi.yassine15@gmail.com>
2021-06-16 00:41:34 +0100

    testimonial modified

--- webProject/src/app/services/auth-service.service.ts
+++ webProject/src/app/services/auth-service.service.ts
@@ -1,16 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor(private HttpClient) { }
-/*
-  login(credentials):Observable<any> {
-    this.HttpClient.post(API URL here)
+  constructor(private HttpClient:HttpClient) { }
+  
+  login(credentials): Observable<any> {
+   return this.HttpClient.post("https://virtserver.swaggerhub.com/eya-cfu/jobsWebsite/1.0/employers/5", credentials )
   }
-  */
+  
  isAuthentificated():boolean{
    return !!localStorage.getItem('token');
  }
--- webProject/src/app/sign-in/sign-in.component.ts
+++ webProject/src/app/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthServiceService } from '../services/auth-service.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -9,12 +10,12 @@ import { Router } from '@angular/router';
 })
 export class SignInComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private authService:AuthServiceService) { }
 
   ngOnInit(): void {
   }
 
- /*onSubmit( loginForm: NgForm){
+ onSubmit( loginForm: NgForm){
     this.authService.login(loginForm.value).subscribe(
       (data) => {
         localStorage.setItem('token', data['id']);
@@ -22,6 +23,6 @@ export class SignInComponent implements OnInit {
       },
       (erreur) => alert("Veuillez vérifier vos coordonnées")
     )
-  }*/ 
+  }
 
 }
